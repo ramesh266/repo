30b6819137abbd47f6d0d19fe275ba129ece7636
ramesh k <rkanisiri@gmail.com>
2021-05-07 10:35:33 +0000

    commit

--- ram.sh
+++ ram.sh
@@ -1,18 +1,23 @@
 echo "hello world"
 echo "welcome to my program"
 echo " hi"
-
-
-
 echo "hi"
 hello
 hi
 hello
+hello
+hello
+hello
+hello
+hello
 
 
-hello
-hello
-hello
-hello
-hello
+ram
+ram
+ram
+ram
+ram
+ram
+ram
+ram
 
